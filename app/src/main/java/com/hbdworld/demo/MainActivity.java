28e1ef8faec8bd2bc9256f9b2b592f2b585e961d
jinghewang <jinghewang@163.com>
2017-08-22 21:29:05 +0800

    okhttp3

--- app/src/main/java/com/hbdworld/demo/MainActivity.java
+++ app/src/main/java/com/hbdworld/demo/MainActivity.java
@@ -2,14 +2,25 @@ package com.hbdworld.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.btn_bad)
     Button btnBad;
+    @BindView(R.id.tx_url)
+    EditText txUrl;
+    @BindView(R.id.btn_call)
+    Button btnCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +58,65 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, btn.getText(), Toast.LENGTH_SHORT).show();
 
         tvResult.setText(btn.getText());
+    }
+
+    @OnClick({R.id.btn_call})
+    public void onCallClick(View view)  {
+        //this.btnOk = btnOk;
+        Button btn = (Button) view;
+        Toast.makeText(this, btn.getText(), Toast.LENGTH_SHORT).show();
+
+        String result = null;
+        try {
+            get(txUrl.getText().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //tvResult.setText(result);
+    }
+
+    OkHttpClient client = new OkHttpClient();
+
+    private String TAG = this.toString();
+
+    private void get(String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+        //Response response = client.newCall(request).execute();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("okHttp",e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String json = response.body().string();
+                Log.d("okHttp", json);
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(TAG, "code: ");
+                        tvResult.setText(json);
+                    }
+                });
+            }
+        });
+    }
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    private String post(String url, String json) throws IOException {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return response.body().string();
+        } else {
+            throw new IOException("Unexpected code " + response);
+        }
     }
 }
